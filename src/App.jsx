4c5c096d64952f1ebce613852700411f0594e722
TahsinWahid <twahid333@gmail.com>
2026-06-16 13:46:51 +0600

    2nd commit for hero section

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import NavBar from './components/NavBar.jsx'
+import Hero from './components/Hero.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <main>
-      <NavBar/>
+      <NavBar />
+      <Hero />
     </main>
   )
 }
